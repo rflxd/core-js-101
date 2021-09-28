b3ed8cf3affc2479b324cac94ba70dcbea54f884
rflxd <lashakardava@gmail.com>
2021-09-29 00:42:09 +0400

    commit

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -27,8 +27,11 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
 }
 
 
@@ -43,8 +46,8 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  return n <= 1 ? 1 : n * getFactorial(n - 1);
 }
 
 
@@ -60,8 +63,12 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let result = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 
@@ -117,8 +124,18 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  let firstPart;
+  let secondPart;
+  const rect1Left = (rect1.left + rect1.width);
+  const rect2Left = (rect2.left + rect2.width);
+  const rect1Top = (rect1.top + rect1.height);
+  const rect2Top = (rect2.top + rect2.height);
+  if (rect2.left >= rect1Left || rect1.left >= rect2Left) firstPart = false;
+  else firstPart = true;
+  if (rect2.top >= rect1Top || rect1.top >= rect2Top) secondPart = false;
+  else secondPart = true;
+  return firstPart && secondPart;
 }
 
 
